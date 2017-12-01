e2463e4be1c9f34f07f2af07c74665f1d326ac07
“ChrisWen960216 <957638221@qq.com>
2017-12-01 13:10:09 +0800

    Add server

---
+++ _test_/index.js
@@ -0,0 +1,14 @@
+/** Created By ChrisWen
+ *  17/12/01
+ *  react样例
+ */
+
+import React, {Component} from 'react';
+
+export default class TestComponent extends Component {
+  redner(){
+    return (
+      <h1>This is the Test Component!</h1>
+    );
+  }
+}
